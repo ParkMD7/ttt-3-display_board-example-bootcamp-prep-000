dd14f1546eaf735883f1f4f3348560b9d8b6ca59
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-29 17:39:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ def display_board(board)
   puts "A 3x3 Tic Tac Toe Board"
   row = [ "   ", | "   ", | "   "]
   puts "-----------"
-  puts "   ", | "   ", | "   "
+  row = [ "   ", | "   ", | "   "]
   puts "-----------"
-  puts "   ", | "   ", | "   "
+  row = [ "   ", | "   ", | "   "]
 end
